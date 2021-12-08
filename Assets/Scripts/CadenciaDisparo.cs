157ee2be18077bbf1badba98aa4c055bc7053b5a
Ainara Ramos <ainaretework@gmail.com>
2021-12-08 23:38:52 +0100

    Estado final

---
+++ Assets/Scripts/CadenciaDisparo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CadenciaDisparo : MonoBehaviour
+{
+    public GameObject Bala;
+    public GameObject Cañon;
+    public float Tiempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tiempo += Time.deltaTime;
+        if (Tiempo > 4)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject g = Instantiate(Bala, Cañon.transform.position, Cañon.transform.rotation);
+                g.GetComponent<Rigidbody>().AddForce(g.transform.forward * 2000);
+                Tiempo = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CuentaAtras.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuentaAtras : MonoBehaviour
+{
+    public float Speed;
+    public float SpeedRot;
+    public int T;
+   // public CadenciaDisparo refCadenciaDisparo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (T > 0)
+        {
+            T--;
+            MovimientoTanque();
+        }
+        else
+        {
+            Debug.Log("Te has quedado sin gasolina.");
+            StopCoroutine("T");
+        }
+    }
+
+    void MovimientoTanque()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0, 1) * Speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, 0, -1) * Speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * Speed * Time.deltaTime);
+        }
+    }
+}
